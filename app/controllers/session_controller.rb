96c8669e241e81ce19f932a76ed5ef51ed970540
Julie David <julie.angela.david@gmail.com>
2015-11-02 17:48:35 -0500

    Tests passing

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,9 +5,10 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(session_params[:email])
-    if user && user.authenticate(session_params[:password])
-      session[:user_id] = user.id
+
+    @user = User.find_by_email(params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
       flash[:success] = 'Welcome!'
       redirect_to dashboard_path
     else
@@ -20,12 +21,6 @@ class SessionController < ApplicationController
     session[:user_id] = nil
     flash[:alert] = 'Logged out.'
     redirect_to login_path
-  end
-
-  private
-
-  def session_params
-    params.require(:session).permit(:email, :password)
   end
 
 end
