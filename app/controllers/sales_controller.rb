b428cfbd990b2b3cb87450121dcd7f6246a8634b
Blake Strickland <Stricklandb02@gmail.com>
2015-11-07 14:07:30 -0500

    fix lines on sales page

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,15 +5,17 @@ class SalesController < ApplicationController
   after_action :update_goals, only: [:create, :update, :destroy]
   # GET /sales
   def index
-    @project = Project.all
-    @user = User.find(session[:user_id])
-    redirect_to dashboard_user_path(session[:user_id]) if @user != @current_user
-
     # We need a button to show more, either Ajax or navigate to page of next 50 or all
     if params[:search]
-      @sale = Sale.search(params[:search])
+      @project = Project.all
+      @user = User.find(session[:user_id])
+      redirect_to dashboard_user_path(session[:user_id]) if @user != @current_user
+      @sales = Sale.search(params[:search])
     else
-      @sale = Sale.where(project_id: Project.where(user_id: @current_user.id))
+      @project = Project.all
+      @user = User.find(session[:user_id])
+      redirect_to dashboard_user_path(session[:user_id]) if @user != @current_user
+      @sales = Sale.where(project_id: Project.where(user_id: @current_user.id))
     end
   end
 
