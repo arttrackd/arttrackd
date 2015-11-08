7ef9f2471dc8119f3e97586f2ea240951175edc7
Julie David <julie.angela.david@gmail.com>
2015-11-07 23:24:11 -0500

    Sorting out project costs

--- app/controllers/project_costs_controller.rb
+++ app/controllers/project_costs_controller.rb
@@ -5,13 +5,12 @@ class ProjectCostsController < ApplicationController
   # GET /project_costs
   def index
     if params[:search]
-      @projects = Project.includes(:sales, :user, :time_entries).where(user: @current_user)
+      @projects = Project.joins(:sales, :user, :time_entries).where(user: @current_user)
       q = "%#{params[:search]}%"
       pc = ProjectCost.where(project_id: Project.where(user_id: @current_user.id))
       @project_costs = ProjectCost.search(pc, q)
     else
-      @projects = Project.includes(:sales, :user, :time_entries).where(user: @current_user)
-      @project_costs = ProjectCost.where(project_id: Project.where(user_id: @current_user.id))
+      @projects = @current_user.projects
     end
   end
 
