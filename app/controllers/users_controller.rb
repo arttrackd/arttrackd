088818192b059291c63855e2e28dde649ac68866
Julie David <julie.angela.david@gmail.com>
2015-11-07 19:22:04 -0500

    Dashboard does not crash on login if user has no goals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,11 +54,14 @@ class UsersController < ApplicationController
 
   def dashboard
     redirect_to dashboard_user_path(session[:user_id]) unless @user == @current_user
-    @goals = SalesGoal.where(user: @user).limit(5)
-    @goal = SalesGoal.where(user: @user).last
-    @percent_completion = SalesGoal.percent_completion(@goal)
     @projects = Project.where('user_id = ?', @user.id).limit(5)
     @sales = Sale.where(project_id: Project.where(user_id: @user.id)).limit(5)
+    @goals = SalesGoal.where(user: @user).limit(5)
+    @goal = SalesGoal.where(user: @user).last
+    if @user.sales_goals.length > 0
+      @percent_completion = SalesGoal.percent_completion(@goal)
+    else
+    end
   end
 
   private
