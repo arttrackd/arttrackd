4e3a103696e6d7f0233b4a2eee74f2b4c94ad705
Blake Strickland <Stricklandb02@gmail.com>
2015-11-10 14:09:05 -0500

    Fix material uses tests

--- test/models/material_use_test.rb
+++ test/models/material_use_test.rb
@@ -2,23 +2,22 @@ require 'test_helper'
 
 class MaterialUseTest < ActiveSupport::TestCase
   test "search finds relevant information" do
-    mu1 = MaterialUse.create!(name: "Iron", description: "Used up", units: 50)
-    mu2 = MaterialUse.create!(name: "Wood", description: "It's gone now.", units: 2500)
+    project1 = Project.create!(user: users(:one), name: "Cellar Door",
+      description: "The most beautiful phrase in the English language")
+    mu1 = MaterialUse.create!(project_id: 1, name: "Iron", description: "Used up", units: 50)
+    mu2 = MaterialUse.create!(project_id: 1, name: "Wood", description: "It's gone now.", units: 2500)
     pc = MaterialUse.all
 
     #Check that names are searched
-    results = MaterialUse.search("Iron")
-    assert results.include?(mu1)
-    refute results.include?(mu2)
+    results = MaterialUse.search(pc, "Cellar")
+    assert results.include?(project1)
 
     #Check that search is case insensitive
-    results = MaterialUse.search("Gone")
-    refute results.include?(mu1)
-    assert results.include?(mu2)
+    results = MaterialUse.search(pc, "Gone")
+    refute results.include?(project1)
 
     #Check that unfound items don't return results
-    results = MaterialUse.search("West")
-    refute results.include?(mu1)
-    refute results.include?(mu2)
+    results = MaterialUse.search(pc, "West")
+    refute results.include?(project1)
   end
 end
