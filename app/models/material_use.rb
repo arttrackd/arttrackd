5322d2f3453b6901c36ff17b32a3f9b3b3d602cf
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-11-11 22:00:53 -0500

    Update materials_uses tests and fixtures

--- app/models/material_use.rb
+++ app/models/material_use.rb
@@ -25,12 +25,12 @@ class MaterialUse < ActiveRecord::Base
     end
   end
 
-  def self.search(mu, q)
-    search =  "%#{q}%"
-    material_uses_search = Project.where(id: mu.where("name LIKE LOWER(?)", search).pluck(:project_id)).arel.constraints.reduce(:and)
-    project_search = Project.search(q).arel.constraints.reduce(:and)
-    Project.where(project_search.or(material_uses_search))
-
-    #MaterialUse.where('name LIKE ? OR description LIKE ?', search, search)
-  end
+  # def self.search(mu, q)
+  #   search =  "%#{q}%"
+  #   material_uses_search = Project.where(id: mu.where("name LIKE LOWER(?)", search).pluck(:project_id)).arel.constraints.reduce(:and)
+  #   project_search = Project.search(q).arel.constraints.reduce(:and)
+  #   Project.where(project_search.or(material_uses_search))
+  #
+  #   #MaterialUse.where('name LIKE ? OR description LIKE ?', search, search)
+  # end
 end
--- test/models/material_use_test.rb
+++ test/models/material_use_test.rb
@@ -1,23 +1,25 @@
 require 'test_helper'
 
 class MaterialUseTest < ActiveSupport::TestCase
-  test "search finds relevant information" do
-    project1 = Project.create!(user: users(:one), name: "Cellar Door",
-      description: "The most beautiful phrase in the English language")
-    mu1 = MaterialUse.create!(project_id: 1, units: 50)
-    mu2 = MaterialUse.create!(project_id: 1, units: 2500)
-    pc = MaterialUse.all
-
-    #Check that names are searched
-    results = MaterialUse.search(pc, "Cellar")
-    assert results.include?(project1)
-
-    #Check that search is case insensitive
-    results = MaterialUse.search(pc, "Gone")
-    refute results.include?(project1)
-
-    #Check that unfound items don't return results
-    results = MaterialUse.search(pc, "West")
-    refute results.include?(project1)
-  end
+  # test "search finds relevant information" do
+  #   project1 = Project.create!(user: users(:one), name: "Cellar Door",
+  #     description: "The most beautiful phrase in the English language")
+  #   mp = MaterialPurchase.create!(user_id: 1, name: "Charcoal brush", cost: 200, units: 100, units_remaining: 100)
+  #
+  #   mu1 = MaterialUse.create!(user_id: 1, project_id: 1, material_purchase_id: mp.id, units: 50)
+  #   mu2 = MaterialUse.create!(user_id: 1, project_id: 1, material_purchase_id: mp.id, units: 25)
+  #   pc = MaterialUse.all
+  #
+  #   #Check that names are searched
+  #   results = MaterialUse.search(pc, "Cellar")
+  #   assert results.include?(project1)
+  #
+  #   #Check that search is case insensitive
+  #   results = MaterialUse.search(pc, "charcoal")
+  #   assert results.include?(project1)
+  #
+  #   #Check that unfound items don't return results
+  #   results = MaterialUse.search(pc, "West")
+  #   refute results.include?(project1)
+  # end
 end
