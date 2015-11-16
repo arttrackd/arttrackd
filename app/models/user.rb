6036129213c9527ce365c754cd3d61374458ca14
Julie David <julie.angela.david@gmail.com>
2015-11-16 09:57:05 -0500

    Favicon

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :project_costs
   has_many :sales
   has_secure_password
-  has_attached_file :avatar, styles: { medium: "300x300>#", thumb: "100x100>" }, default_url: '/images/track.png'
+  has_attached_file :avatar, styles: { medium: "300x300>#", thumb: "100x100>" }, default_url: 'track.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true
   validates :email, presence: true,
