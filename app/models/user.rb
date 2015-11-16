d0b1a0e7d2df8bd1d03dc9c03b9f9ccc3e8a7c15
Julie David <julie.angela.david@gmail.com>
2015-11-16 14:55:08 -0500

    Attr accessor avatar file name on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :project_costs
   has_many :sales
   has_secure_password
-  has_attached_file :avatar, styles: { medium: "300x300>#", thumb: "100x100>" } 
+  has_attached_file :avatar, styles: { medium: "300x300>#", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true
   validates :email, presence: true,
@@ -26,5 +26,5 @@ class User < ActiveRecord::Base
       }
   validates :password, length: { minimum: 8 }, allow_nil: true
   validates :hourly_rate, presence: true, numericality: {greater_than: 0}
-
+  attr_accessor :avatar_file_name
 end
